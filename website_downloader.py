0eb84946972dbc66e8be9a50152aa55efc7f36a7
Oleg <GreyCatProductionsOriginal@gmail.com>
2025-01-19 09:32:15 +0100

    Better failsafes

--- website_downloader.py
+++ website_downloader.py
@@ -1,16 +1,42 @@
-import os.path
+import os
+from time import sleep
 import requests
 
-counter = 0
 
 def download_page(url, save_location):
-    global counter
-    counter += 1
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(os.path.join(save_location, f"site({counter}).html"), "w", encoding="utf-8") as file:
-            file.write(response.text)
-        return True
+    response = get_response(url)
+
+    if response and response.status_code == 200:
+        try:
+            os.makedirs(save_location, exist_ok=True)
+
+            counter = len(os.listdir(save_location)) + 1
+            file_path = os.path.join(save_location, f"site({counter}).html")
+
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write(response.text)
+            return True
+        except Exception as e:
+            return False
     else:
-        print(f"Failed to retrieve the website. Status code: {response.status_code}")
+        print(f"Failed to retrieve the website after multiple attempts.")
         return False
+
+def get_response(url):
+    retries = 5
+
+    while retries > 0:
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            return response
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            print(f"Request timed out or connection error for URL: {url}")
+            sleep(300)
+        except Exception as e:
+            print(f"Failed to download page {url}: {e}")
+        retries -= 1
+        if retries > 0:
+            print(f"Retrying... ({retries} retries left)")
+            sleep(10)
+    return None
